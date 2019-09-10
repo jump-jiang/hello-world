1d6bf377f482130de7a2ffdb1b3d764013f3bb0e
Jump <753029781@qq.com>
2019-09-10 08:47:59 +0800

    1111

---
+++ consumer/src/main/java/cn/kgc/consumer/controller/UserController.java
@@ -0,0 +1,22 @@
+package cn.kgc.consumer.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping("/buy")
+    public String buyTicket(String name){
+        String s=restTemplate.getForObject("http://PROVIDER-TICKER/ticket",String.class);
+        return name+"购买力"+s;
+    }
+}
